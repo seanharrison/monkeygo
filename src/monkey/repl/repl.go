ae27ecd949ae6222dfe8df7e0ef8c4ff931aef59
Sean Harrison <sah@bookgenesis.com>
2020-04-05 22:52:35 -0500

    § 2.9 – RPPL

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = "🐵> " // monkey see, monkey do
+const ERR_PROMPT = "\t🐒! "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,10 +23,21 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		lex := lexer.New(line)
+		p := parser.New(lex)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, ERR_PROMPT+msg+"\n")
 	}
 }
