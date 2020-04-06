1557b0643bbb6e50fa416e88a95c4d2415c8b0d1
Sean Harrison <sah@bookgenesis.com>
2020-04-05 20:18:02 -0500

    § 2.6 Prefix Expressions

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -149,3 +149,26 @@ func (i *IntegerLiteral) String() string {
 	return i.Token.Literal
 }
 func (i *IntegerLiteral) expressionNode() {}
+
+// PrefixExpression
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
